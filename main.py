ac7dbc28146d962753d5032824c4acce21f5c0b3
Yevhenii Hordashnyk <yevhenii.hordashnyk@swarmingtech.com>
2017-11-13 01:11:03 +0200

    Add help page, add ability to set multiple links

--- main.py
+++ main.py
@@ -6,7 +6,17 @@ from lxml import etree
 import requests
 import functools
 import datetime
+import argparse
 from tabulate import tabulate
+
+
+parser = argparse.ArgumentParser(description='Asynchronous Magento Cache Warmer')
+parser.add_argument('-s','--site', action="append", dest='sites', default=None)
+parser.add_argument('-l','--limit', action="store", dest='limit', default=None)
+args = parser.parse_args()
+limit = args.limit
+sites = args.sites
+
 
 ## Colorize ##
 red='\033[0;31m'
@@ -16,9 +26,13 @@ no_color='\033[0m'
 tasks = []
 results=[]
 time_array=[]
+failed_links=0
+success_links=0
 
 def get_links(mage_links):
     r = requests.get(mage_links)
+    if "200" not in str(r):
+        sys.exit(red + "Sitemap fetch failed for %s with %s. Exiting..." % (mage_links, r) + no_color)
     root = etree.fromstring(r.content)
     print ("The number of sitemap tags are %s" % str((len(root))) )
     links=[]
@@ -27,12 +41,15 @@ def get_links(mage_links):
         links.append(children[0].text)
     return links
 
+
+async def bound_warms(sem, url):
+    async with sem:
+        await warm_it(url)
+
 async def warm_it(url):
 
     connection_started_time = None
     connection_made_time = None
-    global failed_links
-    global success_links
 
     class TimedResponseHandler(aiohttp.client_proto.ResponseHandler):
         def connection_made(self, transport):
@@ -51,29 +68,41 @@ async def warm_it(url):
             return await super()._create_connection(req)
 
     async with aiohttp.ClientSession(connector=TimedTCPConnector(loop=loop)) as session:
-        failed_links = 0
-        success_links = 0
         async with session.get(url) as response:
             time_delta=connection_made_time - connection_started_time
             time_taken="%s sec %s microsec" % (time_delta.seconds, time_delta.microseconds)
             if response.status!= 200:
                 response_output=red + str(response.status) + no_color
-                failed_links+=1
             else:
                 response_output=green + str(response.status) + no_color
-                success_links+=1
                 time_array.append(time_delta)
             results.append([url, response_output, time_taken])
+            print('.', end='', flush=True)
 
-loop = asyncio.get_event_loop()
+if limit == None:
+    print ("The concurrency limit isn't specified. Setting limit to 150")
+    limit=150
+else:
+    print ("Setting concurrency limit to %s" % (limit))
 
-mage_links=get_links(sys.argv[1])
+sem = asyncio.Semaphore(int(limit))
 
-for i in mage_links:
-    task = asyncio.ensure_future(warm_it(i))
-    tasks.append(task)
-loop.run_until_complete(asyncio.wait(tasks))
+for site in sites:
+    print ("#############################################################################################")
+    print ("Processing %s" % (site))
+    loop = asyncio.get_event_loop()
+    mage_links=get_links(str(site))
+    for i in mage_links:
+        task = asyncio.ensure_future(bound_warms(sem, i))
+        tasks.append(task)
+    loop.run_until_complete(asyncio.wait(tasks))
 
-avg_time = str((sum([ x.total_seconds() for x in time_array]))/len(time_array))
-print (tabulate(results, headers=['URL', 'Response code', 'Time']))
-print (tabulate([[str(failed_links), str(success_links), avg_time]], headers=['Failed', 'Successfull', 'Average Time']))
+    for i in results:
+        if "200" in i[1]:
+            success_links+=1
+        else:
+            failed_links+=1
+
+    avg_time = str((sum([ x.total_seconds() for x in time_array]))/len(time_array))
+    print (tabulate(results, headers=['URL', 'Response code', 'Time']))
+    print (tabulate([[str(failed_links), str(success_links), avg_time]], headers=['Failed', 'Successfull', 'Average Time']))
